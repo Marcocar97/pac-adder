07e1138b29199de7949c25b5676dc5d0b8acf47e
Marcos <mmarkito708@gmail.com>
2024-07-04 13:18:54 +0200

    colisiones entre elementos solucionadas, modos de juegos activados

--- js/main.js
+++ js/main.js
@@ -9,36 +9,43 @@ const gameBoxNode = document.querySelector("#game-box");
 
 // AUDIOS
 
-const collisionSound = document.querySelector("#choque")
-const recogerSoun = document.querySelector("#recogida")
-const movingSound = document.querySelector("#movimieno")
-movingSound.playbackRate = 2.0
+const collisionSound = document.querySelector("#choque");
+const recogerSoun = document.querySelector("#recogida");
+const movingSound = document.querySelector("#movimieno");
 
+movingSound.playbackRate = 2.0;
+
+collisionSound.volume = 0.05;
+recogerSoun.volume = 0.05;
+movingSound.volume = 0.05;
 
 // BOTONES
 const startBtnNode = document.querySelector("#start-btn");
 const resetBtnNode = document.querySelector("#reset-btn");
+const easyModeBtn = document.querySelector("#easyMode")
+const normalModeBtn = document.querySelector("#normalMode")
 
 // ELEMENTOS DEL GAME SCREEN
 
 const scoreNode = document.querySelector("#score");
-const highscoreNode = document.querySelector("#highscore")
+const highscoreNode = document.querySelector("#highscore");
 
-let score = 0
-let highscore = 0
-
+let score = 0;
+let highscore = 0;
 
 // VARIABLES GLOBALES
 
 let pacmanOb = null;
-//let obstaculoObj = null
+let obstaculoObj = 0
 let obstaculoArr = [];
 //let bolaObj = null
 let bolasArr = [];
 let nextObst = 3;
 let IntervalId1 = null;
 
-let bordeBox = gameBoxNode.offsetWidth
+let NormalMode = true
+
+let bordeBox = gameBoxNode.offsetWidth;
 
 // FUNCIONES GLOBALES
 
@@ -47,14 +54,15 @@ function startGame() {
 
   gameScreenNode.style.display = "flex";
 
-  score = 0
+  score = 0;
+
+  NormalMode = true
 
   pacmanOb = new Pacman();
 
-
   obstaculoArr = [];
   bolasArr = [];
-  nextObst = 3
+  nextObst = 3;
 
   bolasAppear();
 
@@ -68,55 +76,28 @@ function gameLoop() {
   bolaCollision();
   obstaculoColission();
   bordesColission();
-
-  // VERIFICAR COLISSION
-  resolveCollisions(obstaculoArr, bolasArr); 
-  resolveCollisions(bolasArr, obstaculoArr); 
-
 }
-
-
-
-// VERIFICAR COLISION
-
-function isColliding(obj1, obj2) { 
-    if ( obj1.x < obj2.x + obj2.w && 
-        obj1.x + obj1.w > obj2.x && 
-        obj1.y < obj2.y + obj2.h && 
-        obj1.y + obj1.h > obj2.y ) { 
-
-obj1.x = Math.random() * 400; obj1.y = Math.random() * 400; } } 
-
-// Función para resolver colisiones entre obstaculoArr y bolasArr 
-
- function resolveCollisions(arr1, arr2) { 
-    arr1.forEach(obj1 => { 
-        arr2.forEach(obj2 => { 
-       if ( isColliding(obj1, obj2)){console.log("detectado")}; }); }); } 
-
-
-// VERIFICAR COLISION
-
 
 function obstaculoAppear() {
   let randomPosititioY = Math.random() * 400;
 
   let randomPositionX = Math.random() * 400;
 
+  obstaculoObj = new Obstaculos(randomPosititioY, randomPositionX);
+  // si esta en la misma posision que pancman se cambia la posicion
+  while (
+    pacmanOb.x < obstaculoObj.x + obstaculoObj.w &&
+    pacmanOb.x + pacmanOb.w > obstaculoObj.x &&
+    pacmanOb.y < obstaculoObj.y + obstaculoObj.h &&
+    pacmanOb.y + pacmanOb.h > obstaculoObj.y
+  ) {
+    obstaculoObj.x = Math.random() * 400;
+    obstaculoObj.y = Math.random() * 400;
+    obstaculoObj.node.style.left = `${obstaculoObj.x}px`
+    obstaculoObj.node.style.top = `${obstaculoObj.y}px` 
+  } 
+    obstaculoArr.push(obstaculoObj);
 
-  let obstaculoObj = new Obstaculos(
-    randomPosititioY,
-    randomPositionX
-  );
-  // si esta en la misma posision que panda se cambia la posicion
- if (
-      pacmanOb.x < obstaculoObj.x + obstaculoObj.w &&
-      pacmanOb.x + pacmanOb.w > obstaculoObj.x &&
-      pacmanOb.y < obstaculoObj.y + obstaculoObj.h &&
-      pacmanOb.y + pacmanOb.h >obstaculoObj.y
-    ) {randomPositionX = Math.random() * 400; randomPosititioY = Math.random() * 400
-    } else { 
- obstaculoArr.push(obstaculoObj)}
 }
 
 function bolasAppear() {
@@ -125,6 +106,19 @@ function bolasAppear() {
   let randomPositionBolaX = Math.random() * 400;
 
   let bolaObj = new Bolas(randomPosititionBolaY, randomPositionBolaX);
+  while (
+    bolaObj.x < obstaculoObj.x + obstaculoObj.w &&
+    bolaObj.x + bolaObj.w > obstaculoObj.x &&
+    bolaObj.y < obstaculoObj.y + obstaculoObj.h &&
+    bolaObj.y + bolaObj.h > obstaculoObj.y
+  ) {
+    bolaObj.x = Math.random() * 400;
+    bolaObj.y = Math.random() * 400;
+    bolaObj.node.style.left = `${bolaObj.x}px`
+    bolaObj.node.style.top = `${bolaObj.y}px` 
+  } 
+
+
   bolasArr.push(bolaObj);
 }
 
@@ -136,27 +130,25 @@ function bolaCollision() {
       pacmanOb.y < eachBola.y + eachBola.h &&
       pacmanOb.y + pacmanOb.h > eachBola.y
     ) {
-        recogerSoun.play()
+      recogerSoun.play();
 
-        // SCORE
-      score++
-      scoreNode.innerText = score
+      // SCORE
+      score++;
+      scoreNode.innerText = score;
 
       // highscore
 
-      if(score > highscore)
-      highscore = score
-      highscoreNode.innerText = highscore    
+      if (score > highscore) highscore = score;
+      highscoreNode.innerText = highscore;
 
       bolasArr.splice(index, 1);
-      eachBola.node.remove()
+      eachBola.node.remove();
       bolasAppear();
-      
+
       if (scoreNode.innerText >= nextObst) {
         obstaculoAppear();
         nextObst += 3;
-        pacmanOb.pacmanSpeed++
-        
+        pacmanOb.pacmanSpeed++;
       }
     }
   });
@@ -171,20 +163,37 @@ function obstaculoColission() {
       pacmanOb.y + pacmanOb.h > eachObstaculo.y
     ) {
       gameOver();
-
     }
   });
 }
 
 function bordesColission() {
+  if (NormalMode){ 
   if (pacmanOb.x < 0) {
     gameOver();
   } else if (pacmanOb.x > 560) {
     gameOver();
-  } else if (pacmanOb.y < 0) {
+  } 
+  if (pacmanOb.y < 0) {
     gameOver();
-  } else if (pacmanOb.y > 560) gameOver();
+  } else if (pacmanOb.y > 560) {
+    gameOver();
+  }
+} else {
+  if (pacmanOb.x < 0) {
+    pacmanOb.x = 560
+  } else if (pacmanOb.x > 560) {
+    pacmanOb.x = 0
+  } else if (pacmanOb.y < 0) {
+    pacmanOb.y = 560
+  } else if (pacmanOb.y > 560) {
+    pacmanOb.y = 0
+  }
+
 }
+
+}
+
 
 function gameOver() {
   clearInterval(IntervalId1);
@@ -193,12 +202,14 @@ function gameOver() {
 
   pacmanOb.node.remove();
   gameBoxNode.innerHTML = null;
+  scoreNode.innerText = 0
 
-  collisionSound.play()
+  collisionSound.play();
 
+  NormalMode = true
+  normalModeBtn.classList.add("active")
+  easyModeBtn.classList.remove("active")
 }
-
-
 
 // GAME LOOP INTERVAL
 
@@ -212,23 +223,35 @@ window.addEventListener("keydown", (event) => {
   if (event.code === "ArrowUp") {
     pacmanOb.orientacion = "arriba";
     pacmanOb.node.src = "./images/Arriba.png";
-    movingSound.play()
+    movingSound.play();
   } else if (event.code === "ArrowDown") {
     pacmanOb.orientacion = "abajo";
     pacmanOb.node.src = "./images/Abajo.png";
-    movingSound.play()
+    movingSound.play();
   } else if (event.code === "ArrowLeft") {
     pacmanOb.orientacion = "izquierda";
     pacmanOb.node.src = "./images/Izquierda.png";
-    movingSound.play()
+    movingSound.play();
   } else if (event.code === "ArrowRight") {
     pacmanOb.orientacion = "derecha";
     pacmanOb.node.src = "./images/Pacman.png";
-    movingSound.play()
+    movingSound.play();
   }
 });
 
 resetBtnNode.addEventListener("click", () => {
   gameOverScreenNode.style.display = "none";
   startGame();
+});
+
+easyModeBtn.addEventListener("click", () => {
+  NormalMode = false
+  easyModeBtn.classList.add("active")
+  normalModeBtn.classList.remove("active")
+});
+
+normalModeBtn.addEventListener("click", () => {
+  NormalMode = true
+  normalModeBtn.classList.add("active")
+  easyModeBtn.classList.remove("active")
 });
